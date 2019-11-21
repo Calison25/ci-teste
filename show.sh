20ba794682c4fc1d7f00ad7a319fac23bbbaf1b6
Calison <calisonsi@gmail.com>
2019-11-21 11:12:30 -0300

    Modifica a forma como executa o arquivo

---
+++ show.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COLOR_CYAN='\033[0;36m'
+NO_COLOR='\033[0m'
+
+echo -e "${COLOR_CYAN}############################################################${NO_COLOR}"
+echo -e "${COLOR_CYAN}# TESTANDO SE FUNCIONOU#${NO_COLOR}"
+echo -e "${COLOR_CYAN}############################################################${NO_COLOR}"
